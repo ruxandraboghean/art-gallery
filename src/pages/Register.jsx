526789690c16626df0196f71a38177be6e1cc882
Ruxandra Boghean <ruxandraboghean@gmail.com>
2023-02-02 17:09:34 +0200

    init

---
+++ src/pages/Register.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import logo from '../images/logo-default.png';
+import background from '../images/background.png';
+
+export const Register = () => {
+  return (
+    <>
+    <div className='form-container'>
+        <div className='form-wrapper center'>
+            <div className='center'>
+                <img src={logo} alt="Logo" className='logo' /> 
+            </div>
+            
+            <div className='progress-bar'>
+              
+            </div>
+            <form className='form'>
+                <input type="text" placeholder='&#xF007;&nbsp;&nbsp; username' className='form-input' />
+                <input type="email" placeholder='&#xf0e0;&nbsp;&nbsp; email' className='form-input' />
+                <input type="password" placeholder='&#xf023;&nbsp;&nbsp; password'  className='form-input' />
+                <input type="password" placeholder='&#xf023;&nbsp;&nbsp; confirm password'  className='form-input' />
+                <button className='form-button'> Next </button>
+            </form>
+            <p className='form-paragraph'> Already have an account? <a href="/login"> Login </a> </p>
+        </div>
+        <div className='image-wrapper'>
+            <img src={background} alt='Background' className='background' />
+        </div>
+    </div>
+    </>
+  )
+}
