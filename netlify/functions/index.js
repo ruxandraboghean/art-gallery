526befe334d65e398cd52be6594c820427b21c00
Ruxandra Boghean <ruxandraboghean@gmail.com>
2023-07-01 10:56:08 +0300

    'changes

---
+++ netlify/functions/index.js
@@ -0,0 +1,11 @@
+import express from "express";
+import serverless from "serverless-http";
+
+const api = express();
+
+const router = express.Router();
+router.get("/hello", (req, res) => res.send("Hello World!"));
+
+api.use("/api/", router);
+
+export const handler = serverless(api);
